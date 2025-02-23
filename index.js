897ad8ffe7221903bd37b2d5ad66934006205baa
jill bhatt <bhattjill@gmail.com>
2025-02-23 17:08:57 +0530

    Init apollo server and connect with express app

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const { PORT } = require('./config/env');
 
+// apollo imports
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+
 const app = express();
 
 app.use(express.json());
@@ -9,6 +13,33 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.listen(PORT, () => {
-    console.log(`🚀🚀🚀...GraphQL server is live on port: ${PORT}...🚀🚀🚀`);
+const apolloServer = new ApolloServer({
+    typeDefs: `#graphql
+        type Query {
+            hello: String!
+        }
+    `,
+    resolvers: {
+        Query: {
+            hello: () => `Hello World!!`
+        }
+    },
+    introspection: true
 });
+
+apolloServer
+    .start()
+    .then(() => {
+        app.use('/graphql', expressMiddleware(apolloServer));
+
+        app.listen(PORT, () => {
+            console.log(
+                `🚀🚀🚀...GraphQL server is live on port: ${PORT}...🚀🚀🚀`
+            );
+        });
+    })
+    .catch(error => {
+        console.log(
+            `❌❌❌...Apollo server failed to start. Error: ${error.message}...❌❌❌`
+        );
+    });
